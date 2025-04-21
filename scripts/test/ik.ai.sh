9f6bed6b7c2d17313185a042f63d90217adf6cfa
hu.po <hugo.p.cmu@gmail.com>
2025-04-21 12:23:54 -0500

    asd

--- scripts/test/ik.ai.sh
+++ scripts/test/ik.ai.sh
@@ -3,6 +3,6 @@ source "scripts/util/validate_backend.sh"
 docker build -f $TATBOT_ROOT/docker/ik/Dockerfile.$BACKEND -t tatbot-ik-$BACKEND $TATBOT_ROOT
 docker run $GPU_FLAG -it --rm --user="root" \
 tatbot-ik-$BACKEND bash -c "
-source /root/tatbot/.venv/bin/activate && \
-source /root/tatbot/.env && \
-uv run python /root/tatbot/tatbot/ai.py --test"
+source \${TATBOT_ROOT}/.venv/bin/activate && \
+source \${TATBOT_ROOT}/.env && \
+uv run python \${TATBOT_ROOT}/tatbot/ai.py --test"
--- scripts/test/ik.sh
+++ scripts/test/ik.sh
@@ -3,11 +3,11 @@ source "scripts/util/validate_backend.sh"
 docker build -f $TATBOT_ROOT/docker/ik/Dockerfile.$BACKEND -t tatbot-ik-$BACKEND $TATBOT_ROOT
 docker run $GPU_FLAG -it --rm --user="root" \
 tatbot-ik-$BACKEND bash -c "
-source ${TATBOT_ROOT}/.venv/bin/activate && \
-source ${TATBOT_ROOT}/.env && \
+source \${TATBOT_ROOT}/.venv/bin/activate && \
+source \${TATBOT_ROOT}/.env && \
 uv pip freeze && \
-bash ${TATBOT_ROOT}/scripts/util/specs.sh && \
+bash \${TATBOT_ROOT}/scripts/util/specs.sh && \
 if [ ! -z \"\${NVIDIA_VISIBLE_DEVICES:-}\" ]; then
-    uv run python ${TATBOT_ROOT}/tatbot/util/cuda_device_properties.py
+    uv run python \${TATBOT_ROOT}/tatbot/util/cuda_device_properties.py
 fi && \
-uv run python ${TATBOT_ROOT}/tatbot/ik/test.py"
+uv run python \${TATBOT_ROOT}/tatbot/ik/test.py"
