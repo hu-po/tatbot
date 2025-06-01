a37e356784f5ec4e1010983b52cd2028a52906bb
hu.po <hugo.p.cmu@gmail.com>
2025-06-01 15:40:46 -0500

    asd

--- config/realsense.py
+++ config/realsense.py
@@ -1,1 +1,3 @@
+# Realsense should be calibrated out of the box, so probably don't need anything here.
 # https://github.com/IntelRealSense/librealsense/blob/master/wrappers/python/examples/depth_auto_calibration_example.py
+# https://github.com/IntelRealSense/librealsense/blob/master/wrappers/python/examples/depth_ucal_example.py
