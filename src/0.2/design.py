1cdc19f8880a419072f321e0cbadfddb35f01053
hu.po <hugo.p.cmu@gmail.com>
2025-06-09 08:39:54 -0500

    asd

--- src/0.2/design.py
+++ src/0.2/design.py
@@ -1,12 +1,13 @@
+import io
 import os
 import logging
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 import cv2
 import numpy as np
-
 import PIL.Image
 import replicate
+import tyro
 
 logging.basicConfig(
     level=logging.INFO,
@@ -18,24 +19,32 @@ log = logging.getLogger(__name__)
 @dataclass
 class DesignConfig:
     # image_path: str | None = None
-    image_path: str | None = os.path.expanduser("~/tatbot/output/design/cat.png")
+    image_filename: str | None = "cat.png"
     """ (Optional) Local path to the tattoo design image."""
     prompt: str = "cat"
     """ Prompt for the design image generation."""
+    output_dir: str = os.path.expanduser("~/tatbot/output/design")
+    """ Directory to save the design image and patches."""
     image_width_px: int = 2048
     """ Width of the design image (pixels)."""
     image_height_px: int = 2048
     """ Height of the design image (pixels)."""
-    image_width_m: float = 0.04
+    image_width_m: float = 0.06
     """ Width of the design image (meters)."""
-    image_height_m: float = 0.04
+    image_height_m: float = 0.06
     """ Height of the design image (meters)."""
+    num_patches_width: int = 16
+    """ Number of patches along the x-axis."""
+    num_patches_height: int = 16
+    """ Number of patches along the y-axis."""
+
 
 def main(config: DesignConfig):
+    log.info(f"🔍 Using output directory: {config.output_dir}")
+    os.makedirs(config.output_dir, exist_ok=True)
 
-    if config.image_path is None:
-        _prompt = config.prompt.replace(" ", "_")
-        config.image_path = os.path.expanduser(f"~/tatbot/output/design/{_prompt}.png")
+    if config.image_filename is None:
+        image_path: str = f"{config.output_dir}/{config.prompt.replace(' ', '_')}.png"
         log.info(" Generating design...")
         # https://replicate.com/black-forest-labs/flux-1.1-pro-ultra/api/schema
         output = replicate.run(
@@ -49,37 +58,59 @@ def main(config: DesignConfig):
                 "safety_tolerance": 6,
             }
         )
-        with open(config.image_path, "wb") as file:
+        with open(image_path, "wb") as file:
             file.write(output.read())
+    else:
+        image_path = f"{config.output_dir}/{config.image_filename}"
 
-    img_pil = PIL.Image.open(config.image_path)
+    img_pil = PIL.Image.open(image_path)
     original_width, original_height = img_pil.size
     log.info(f"🖼️ Design image size: {original_width}x{original_height} pixels.")
 
-    log.info("🔍 Segmenting design...")
-    # https://replicate.com/meta/sam-2/api/schema
-    output = replicate.run(
-        "meta/sam-2:fe97b453a6455861e3bac769b441ca1f1086110da7466dbb65cf1eecfd60dc83",
-        input={
-            "image": open(config.image_path, "rb"),
-            "points_per_side": 32,
-            "mask_threshold": 0.88,
-        }
-    )
-    import io
+    log.info("Creating patches...")
+    patches_dir = f"{config.output_dir}/{config.image_filename}_patches"
+    os.makedirs(patches_dir, exist_ok=True)
+    log.info(f"Saving patches to {patches_dir}")
 
-    mask_files = output['individual_masks']
-    log.info(f"🔍 Found {len(mask_files)} masks.")
+    patch_num = 0
+    x_coords = np.linspace(0, original_width, config.num_patches_width + 1, dtype=int)
+    y_coords = np.linspace(0, original_height, config.num_patches_height + 1, dtype=int)
 
-    individual_masks_np = [np.array(PIL.Image.open(io.BytesIO(f.read()))) for f in mask_files]
+    for i in range(config.num_patches_height):
+        for j in range(config.num_patches_width):
+            box = (x_coords[j], y_coords[i], x_coords[j + 1], y_coords[i + 1])
+            patch = img_pil.crop(box)
+            patch_path = os.path.join(patches_dir, f"patch_{i:02d}_{j:02d}.png")
+            patch.save(patch_path)
+            patch_num += 1
+    log.info(f"Saved {patch_num} patches.")
 
-    log.info(f"Converted masks to {len(individual_masks_np)} NumPy arrays.")
-    if individual_masks_np:
-        log.info(f"First mask shape: {individual_masks_np[0].shape}")
+    # # TODO: feed each patch to sam-2
+
+    # log.info("🔍 Segmenting design...")
+    # # https://replicate.com/meta/sam-2/api/schema
+    # output = replicate.run(
+    #     "meta/sam-2:fe97b453a6455861e3bac769b441ca1f1086110da7466dbb65cf1eecfd60dc83",
+    #     input={
+    #         "image": open(config.image_path, "rb"),
+    #         "points_per_side": 32,
+    #         "mask_threshold": 0.88,
+    #     }
+    # )
+
+    # mask_files = output['individual_masks']
+    # log.info(f"🔍 Found {len(mask_files)} masks.")
+
+    # individual_masks_np = [np.array(PIL.Image.open(io.BytesIO(f.read()))) for f in mask_files]
+
+    # log.info(f"Converted masks to {len(individual_masks_np)} NumPy arrays.")
+    # if individual_masks_np:
+    #     log.info(f"First mask shape: {individual_masks_np[0].shape}")
 
 
 if __name__ == "__main__":
-    main(DesignConfig())
+    args = tyro.cli(DesignConfig)
+    main(args)
 
 # log.info("🖼️ Loading design...")
 # img_pil = PIL.Image.open(config.image_path)
