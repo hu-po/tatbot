390d4d327bc5dd637779d2c1f22573454ab46257
hu-po <hu.po.xyz@gmail.com>
2025-06-12 09:45:41 -0500

    asd

--- src/0.2/train.sh
+++ src/0.2/train.sh
@@ -1,6 +1,6 @@
 uv run python ~/lerobot/scripts/train.py \
   --policy.path=lerobot/smolvla_base \
-  --dataset.repo_id=hu-po/tatbot-test \
+  --dataset.repo_id=hu-po/tatbot-stencil-1749648845 \
   --output_dir=~/tatbot/outputs/train \
   --batch_size=64 \
   --wandb.enable=true \
