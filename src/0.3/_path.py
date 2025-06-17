47e5436a793824cd0fd5c673a34635ee2a156087
hu-po <hu.po.xyz@gmail.com>
2025-06-17 17:41:55 -0500

    asd

--- src/0.3/_path.py
+++ src/0.3/_path.py
@@ -1,3 +1,4 @@
+import jax.numpy as jnp
 import jax_dataclasses as jdc
 from jaxtyping import Array, Float, Int
 from safetensors.flax import load_file, save_file
@@ -7,14 +8,40 @@ from _log import get_logger
 log = get_logger('_path')
 
 @jdc.pytree_dataclass
+class Path:
+    ee_pos_l: Float[Array, "l 3"]
+    """End effector frame position in meters (x, y, z) for left arm."""
+    ee_pos_r: Float[Array, "l 3"]
+    """End effector frame position in meters (x, y, z) for right arm."""
+    ee_wxyz_l: Float[Array, "l 4"]
+    """End effector frame orientation as quaternion (w, x, y, z) for left arm."""
+    ee_wxyz_r: Float[Array, "l 4"]
+    """End effector frame orientation as quaternion (w, x, y, z) for right arm."""
+    joints: Float[Array, "l 16"]
+    """Joint positions in radians (URDF convention)."""
+    dt: Float[Array, "l 1"]
+    """Travel time from pose N to pose N+1 in seconds."""
+
+    @classmethod
+    def padded(cls, pad_len: int) -> "Path":
+        return cls(
+            ee_pos_l=jnp.zeros((pad_len, 3), dtype=jnp.float32),
+            ee_pos_r=jnp.zeros((pad_len, 3), dtype=jnp.float32),
+            ee_wxyz_l=jnp.full((pad_len, 4), [1.0, 0.0, 0.0, 0.0], dtype=jnp.float32),
+            ee_wxyz_r=jnp.full((pad_len, 4), [1.0, 0.0, 0.0, 0.0], dtype=jnp.float32),
+            joints=jnp.zeros((pad_len, 16), dtype=jnp.float32),
+            dt=jnp.zeros((pad_len, 1), dtype=jnp.float32),
+        )
+
+@jdc.pytree_dataclass
 class PathBatch:
     ee_pos_l: Float[Array, "b l 3"]
     """End effector frame position in meters (x, y, z) for left arm."""
     ee_pos_r: Float[Array, "b l 3"]
     """End effector frame position in meters (x, y, z) for right arm."""
-    wxyz_l: Float[Array, "b l 4"]
+    ee_wxyz_l: Float[Array, "b l 4"]
     """End effector frame orientation as quaternion (w, x, y, z) for left arm."""
-    wxyz_r: Float[Array, "b l 4"]
+    ee_wxyz_r: Float[Array, "b l 4"]
     """End effector frame orientation as quaternion (w, x, y, z) for right arm."""
     joints: Float[Array, "b l 16"]
     """Joint positions in radians (URDF convention)."""
@@ -22,6 +49,18 @@ class PathBatch:
     """Travel time from pose N to pose N+1 in seconds."""
     mask: Int[Array, "b l"]
     """Paths are padded to same length, mask is 1 for valid poses in path."""
+
+    @classmethod
+    def from_paths(cls, paths: list[Path]) -> "PathBatch":
+        return cls(
+            ee_pos_l=jnp.array([path.ee_pos_l for path in paths]),
+            ee_pos_r=jnp.array([path.ee_pos_r for path in paths]),
+            ee_wxyz_l=jnp.array([path.ee_wxyz_l for path in paths]),
+            ee_wxyz_r=jnp.array([path.ee_wxyz_r for path in paths]),
+            joints=jnp.array([path.joints for path in paths]),
+            dt=jnp.array([path.dt for path in paths]),
+            mask=jnp.ones((len(paths), max(len(path.ee_pos_l) for path in paths))),
+        )
 
     def save(self, filepath: str) -> None:
         log.debug(f"💾 Saving PathBatch to {filepath}")
--- src/0.3/_plan.py
+++ src/0.3/_plan.py
@@ -38,6 +38,10 @@ class Plan:
 
     path_pad_len: int = 128
     """Length to pad paths to."""
+    path_dt_fast: float = 0.1
+    """Time between poses in seconds for fast movement."""
+    path_dt_slow: float = 2.0
+    """Time between poses in seconds for slow movement."""
 
     ee_design_pos: tuple[float, float, float] = (0.08, 0.0, 0.04)
     """position of the design ee transform."""
@@ -97,126 +101,74 @@ class Plan:
             log.info(f"⚙️💾 Saving image to {image_path}")
             image.save(image_path)
 
-    def add_pixel_paths(self, paths: list[tuple[int, int]]):
-
-        num_paths = len(paths)
-        num_ink_dips = 1 + num_paths // self.ink_dip_interval
-        total_num_paths = num_paths + num_ink_dips
-
-        ee_pos_l = np.zeros((total_num_paths, self.path_pad_len, 3), dtype=np.float32)
-        ee_pos_r = np.zeros((total_num_paths, self.path_pad_len, 3), dtype=np.float32)
-        wxyz_l = np.zeros((total_num_paths, self.path_pad_len, 4), dtype=np.float32)
-        wxyz_r = np.zeros((total_num_paths, self.path_pad_len, 4), dtype=np.float32)
-        joints = np.zeros((total_num_paths, self.path_pad_len, 16), dtype=np.float32)
-        dt = np.zeros((total_num_paths, self.path_pad_len, 1), dtype=np.float32)
-        mask = np.zeros((total_num_paths, self.path_pad_len), dtype=np.uint8)
+    def add_pixel_paths(self, pixel_paths: list[list[tuple[int, int]]]):
+        num_paths = len(pixel_paths)
+        log.info(f"⚙️ Adding {num_paths} pixel paths...")
 
         scale_x = self.image_width_m / self.image_width_px
         scale_y = self.image_height_m / self.image_height_px
 
-        path_idx = 0
-        for i in range(total_num_paths):
-            _ee_pos_l = []
-            _ee_pos_r = []
-            _wxyz_l = []
-            _wxyz_r = []
-            _dt = []
-            if i % self.ink_dip_interval == 0:
-                self.path_descriptions.append("dip ink")
-                # hover over inkcap
-                _ee_pos_l.append(self.ee_inkcap_pos)
-                _wxyz_l.append(self.ee_inkcap_wxyz)
-                _ee_pos_r.append(self.ee_inkcap_pos + self.view_offset)
-                _wxyz_r.append(self.ee_view_wxyz)
-                _dt.append(3.0)
-                # dip into inkcap
-                _ee_pos_l.append(self.ee_inkcap_pos + self.dip_offset)
-                _wxyz_l.append(self.ee_inkcap_wxyz)
-                _ee_pos_r.append(self.ee_inkcap_pos + self.view_offset)
-                _wxyz_r.append(self.ee_view_wxyz)
-                _dt.append(3.0)
-                # pause inside inkcap
-                _ee_pos_l.append(self.ee_inkcap_pos + self.dip_offset)
-                _wxyz_l.append(self.ee_inkcap_wxyz)
-                _ee_pos_r.append(self.ee_inkcap_pos + self.view_offset)
-                _wxyz_r.append(self.ee_view_wxyz)
-                _dt.append(3.0)
-                # retract from inkcap
-                _ee_pos_l.append(self.ee_inkcap_pos)
-                _wxyz_l.append(self.ee_inkcap_wxyz)
-                _ee_pos_r.append(self.ee_inkcap_pos + self.view_offset)
-                _wxyz_r.append(self.ee_view_wxyz)
-                _dt.append(3.0)
-            else:
-                path_px = paths[path_idx]
-                self.path_descriptions.append(f"path {path_idx} of {num_paths}")
-                path_idx += 1
+        all_paths = []
+        for path_idx, path_px in enumerate(pixel_paths):
+            path = Path.padded(self.path_pad_len)
 
-                pad_len_no_hover = self.path_pad_len - 2
-                if len(path_px) > pad_len_no_hover:
-                    log.warning(f"path {path_idx} has more than {pad_len_no_hover} poses, truncating...")
-                    path_px = path_px[:pad_len_no_hover]
+            self.path_descriptions.append(f"black line path {path_idx} of {num_paths}")
 
-                for pw, ph in path_px:
-                    # pixel coordinates first need to be converted to meters
-                    x_m, y_m = pw * scale_x, ph * scale_y
-                    # center in design frame, add needle offset
-                    _pos_left = [
-                        self.ee_design_pos[0] + x_m - self.image_width_m / 2,
-                        self.ee_design_pos[1] + y_m - self.image_height_m / 2,
-                        self.ee_design_pos[2] + self.needle_offset[2],
-                    ]
-                    _ee_pos_l.append(_pos_left)
-                    _wxyz_l.append(self.ee_design_wxyz)
-                    # right hand just stares at center of design frame
-                    _pos_right = [
-                        self.ee_design_pos[0] + self.view_offset[0],
-                        self.ee_design_pos[1] + self.view_offset[1],
-                        _pos_left[2] + self.view_offset[2],
-                    ]
-                    _ee_pos_r.append(_pos_right)
-                    _wxyz_r.append(self.ee_view_wxyz)
-                # add hover positions to the beginning and end of the path
-                _hover_pos_start_left = [
-                    ee_pos_l[0][0] + self.hover_offset[0],
-                    ee_pos_l[0][1] + self.hover_offset[1],
-                    ee_pos_l[0][2] + self.hover_offset[2],
+            if len(path_px) + 2 > self.path_pad_len:
+                log.warning(f"path {path_idx} has more than {self.path_pad_len} poses, truncating...")
+                path_px = path_px[:self.path_pad_len - 2] # -2 for hover positions
+
+            for i, (pw, ph) in enumerate(path_px):
+                # pixel coordinates first need to be converted to meters
+                x_m, y_m = pw * scale_x, ph * scale_y
+                # center in design frame, add needle offset
+                _pos_left = [
+                    self.ee_design_pos[0] + x_m - self.image_width_m / 2,
+                    self.ee_design_pos[1] + y_m - self.image_height_m / 2,
+                    self.ee_design_pos[2] + self.needle_offset[2],
                 ]
-                _ee_pos_l.insert(0, _hover_pos_start_left)
-                _wxyz_l.insert(0, self.ee_design_wxyz)
-                _hover_pos_end_left = [
-                    ee_pos_l[-1][0] + self.hover_offset[0],
-                    ee_pos_l[-1][1] + self.hover_offset[1],
-                    ee_pos_l[-1][2] + self.hover_offset[2],
+                path.ee_pos_l[i + 1, :] = _pos_left
+                path.ee_wxyz_l[i + 1, :] = self.ee_design_wxyz
+                # right hand just stares at center of design frame
+                _pos_right = [
+                    self.ee_design_pos[0] + self.view_offset[0],
+                    self.ee_design_pos[1] + self.view_offset[1],
+                    _pos_left[2] + self.view_offset[2],
                 ]
-                _ee_pos_l.append(_hover_pos_end_left)
-                _wxyz_l.append(self.ee_design_wxyz)
-                # make sure right hand has same number of poses, but no hover
-                _ee_pos_r.insert(0, _ee_pos_r[0])
-                _wxyz_r.insert(0, _wxyz_r[0])
-                _ee_pos_r.append(_ee_pos_r[-1])
-                _wxyz_r.append(_wxyz_r[-1])
-
-            ee_pos_l[i, :, :] = _ee_pos_l
-            ee_pos_r[i, :, :] = _ee_pos_r
-            wxyz_l[i, :, :] = _wxyz_l
-            wxyz_r[i, :, :] = _wxyz_r
-            joints[i, :, :] = batch_ik(
-                target_wxyz=np.array([wxyz_l, wxyz_r]),
-                target_position=np.array([ee_pos_l, ee_pos_r]),
+                path.ee_pos_r[i + 1, :] = _pos_right
+                path.ee_wxyz_r[i + 1, :] = self.ee_view_wxyz
+            # add hover positions to the beginning and end of the path
+            _hover_pos_start_left = [
+                path.ee_pos_l[0, 0] + self.hover_offset[0],
+                path.ee_pos_l[0, 1] + self.hover_offset[1],
+                path.ee_pos_l[0, 2] + self.hover_offset[2],
+            ]
+            path.ee_pos_l[0, :] = _hover_pos_start_left
+            path.ee_wxyz_l[0, :] = self.ee_design_wxyz
+            path.ee_pos_l[-1, :] = [
+                path.ee_pos_l[-1, 0] + self.hover_offset[0],
+                path.ee_pos_l[-1, 1] + self.hover_offset[1],
+                path.ee_pos_l[-1, 2] + self.hover_offset[2],
+            ]
+            path.ee_wxyz_l[-1, :] = self.ee_design_wxyz
+            # make sure right hand has same number of poses, but no hover
+            path.ee_pos_r[0, :] = path.ee_pos_r[1, :]
+            path.ee_wxyz_r[0, :] = path.ee_wxyz_r[1, :]
+            path.ee_pos_r[-1, :] = path.ee_pos_r[-2, :]
+            path.ee_wxyz_r[-1, :] = path.ee_wxyz_r[-2, :]
+            # compute joint positions
+            path.joints[:, :] = batch_ik(
+                target_wxyz=jnp.array([path.ee_wxyz_l[:, :], path.ee_wxyz_r[:, :]]),
+                target_position=jnp.array([path.ee_pos_l[:, :], path.ee_pos_r[:, :]]),
             )
-            dt[i, :, 0] = dt
-            mask[i, :] = 1
+            # slow movement at the hover positions
+            path.dt[0, 0] = self.path_dt_slow
+            path.dt[1:-1, 0] = self.path_dt_fast
+            path.dt[-1, 0] = self.path_dt_slow
 
-        path_batch = PathBatch(
-            ee_pos_l=ee_pos_l,
-            ee_pos_r=ee_pos_r,
-            wxyz_l=wxyz_l,
-            wxyz_r=wxyz_r,
-            joints=joints,
-            dt=dt,
-            mask=mask,
-        )
+            all_paths.append(path)
+
+        path_batch = PathBatch.from_paths(all_paths)
         path_batch.save(os.path.join(self.dirpath, PATHS_FILENAME))
 
 
