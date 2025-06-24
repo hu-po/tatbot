fd0f0992ef6264b5238e4e9240a5597e99afa202
hu.po <hugo.p.cmu@gmail.com>
2025-06-24 13:22:23 -0500

    asd

--- src/0.3/run_mcp.py
+++ src/0.3/run_mcp.py
@@ -1,39 +1,3 @@
-"""
-TODO:
-
-# server mvp
-# mcp inspector
-# cursor as client (settings add mcp server)
-# add to cursor mcp directory https://cursor.directory/mcp
-
-ideas:
-- ping nodes
-- git pull local tatbot repo, reinstall uv env
-
-- send files to nodes
-- run commands on nodes
-- get plan data
-- get images
-- list available nodes
-- get tatbot info
-- get tech docs
-- get configs
-- generate plan
-- sim control
-- run plan on robot
-- run camera calibration
-- configure robot
-- open up viz browser, chrome, turn on screen
-- kill all python processes, kill all docker containers
-- gradio frontend for tatbot MCP
-- git pull on all machines
-- uv env install on all machines
-- distribute files to all machines
-- ojo: start/stop containers, get CPU/GPU usage, pull latest pattern
-- rpi1: pause/play live viz, set path and pose w/ live viz, reset live viz, open chrome
-- trossen: reset/check realsenses, configure robot, run bot with CLI kwargs(0.3) 
-
-"""
 import concurrent.futures
 from dataclasses import dataclass
 import json
