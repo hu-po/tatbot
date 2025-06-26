b9e37d86db3e4a2d213a656381a3759b13874459
hu.po <hugo.p.cmu@gmail.com>
2025-06-26 15:09:24 -0500

    asd

--- src/gen_sketch.py
+++ src/gen_sketch.py
@@ -1,1 +1,5 @@
-# TODO: generate plan from swiftsketch
+# TODO: generate plans from:
+
+- https://github.com/SonarSonic/DrawingBotV3
+- https://github.com/brunoimbrizi/svg-to-poly
+- https://swiftsketch.github.io/
