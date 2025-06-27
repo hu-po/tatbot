918e27df5c4d341a45e23ab7918c14070f400df3
hu.po <hugo.p.cmu@gmail.com>
2025-06-27 15:49:13 -0500

    asd

--- src/new/data.py
+++ src/new/data.py
@@ -1,6 +1,6 @@
 # shared dataclasses for all modules, no jaxdc
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 @dataclass
 class BotConfig:
@@ -15,6 +15,91 @@ class BotConfig:
         1.0, 0.1, 0.5, -1.2, 0.0, 0.0, 0.0, 0.0, # right arm
     )
     """Rest pose for the robot."""
+
+@dataclass
+class Node:
+    name: str
+    """Name of the node (a computer within the tatbot network)."""
+    ip: str
+    """IP address of the node, used for SSH connection."""
+    user: str
+    """Username for SSH connection."""
+    emoji: str = "🌐"
+    """Emoji to use for logging."""
+    deps: str = "."
+    """Dependencies to install on the node, see pyproject.toml."""
+
+@dataclass
+class InkCap:
+    """Individual cylindrical inkcap."""
+    urdf_link_name: str = "inkcap_large"
+    """URDF link name of the inkcap."""
+    diameter_m: float = 0.008
+    """Diameter of the inkcap (meters)."""
+    depth_m: float = 0.01
+    """Depth of the inkcap (meters)."""
+    color: str = "black"
+    """Natural language description of the color of the ink inside the inkcap."""
+
+@dataclass
+class InkPalette:
+    urdf_link_name: str = "inkpalette"
+    """URDF link name of the inkpalette."""
+    inkcaps: dict[str, InkCap] = field(default_factory=lambda: {
+        "small_1": InkCap(
+            urdf_link_name="inkcap_small_1",
+            color="pink"
+        ),
+        "large": InkCap(
+            urdf_link_name="inkcap_large",
+            diameter_m=0.014,
+            depth_m=0.014,
+            color="black"
+        ),
+        "small_2": InkCap(
+            urdf_link_name="inkcap_small_2",
+            color="blue"
+        ),
+        "small_3": InkCap(
+            urdf_link_name="inkcap_small_3",
+            color="white"
+        ),
+        "medium_1": InkCap(
+            urdf_link_name="inkcap_medium_1",
+            diameter_m=0.012,
+            color="red"
+        ),
+        "medium_2": InkCap(
+            urdf_link_name="inkcap_medium_2",
+            diameter_m=0.012,
+            color="green"
+        ),
+    })
+
+@dataclass
+class TagConfig:
+    family: str = "tag16h5"
+    """Family of AprilTags to use."""
+    size_m: float = 0.041
+    """Size of AprilTags: distance between detection corners (meters)."""
+    enabled_tags: dict[int, str] = field(default_factory=lambda: {
+        6: "arm_l",
+        7: "arm_r",
+        9: "palette",
+        10: "origin",
+        11: "skin",
+    })
+    """ Dictionary of enabled AprilTag IDs."""
+    urdf_link_names: dict[int, str] = field(default_factory=lambda: {
+        6: "tag6",
+        7: "tag7",
+        9: "tag9",
+        10: "tag10",
+        11: "tag11",
+    })
+    """ Dictionary of AprilTag IDs to URDF link names."""
+    decision_margin: float = 20.0
+    """Minimum decision margin for AprilTag detection filtering."""
 
 @dataclass
 class CameraIntrinsics:
@@ -35,3 +120,100 @@ class CameraIntrinsics:
 class CameraExtrinsics:
     pos: tuple[float, float, float] = (0.0, 0.0, 0.0)
     wxyz: tuple[float, float, float, float] = (1.0, 0.0, 0.0, 0.0)
+
+@dataclass
+class Scan:
+    name: str = "scan"
+    """Name of the scan."""
+
+    bot_config: BotConfig = field(default_factory=BotConfig)
+    """Bot configuration to use for the scan."""
+    ink_config: InkConfig = field(default_factory=InkConfig)
+    """Config containig InkCaps and palette position."""
+    tag_config: TagConfig = field(default_factory=TagConfig)
+    """Config containing AprilTag parameters."""
+
+    optical_frame_urdf_link_names: dict[str, str] = field(default_factory=lambda: {
+        "realsense1": "realsense1_color_optical_frame",
+        "realsense2": "realsense2_color_optical_frame",
+        "camera1": "camera1_optical_frame",
+        "camera2": "camera2_optical_frame",
+        "camera3": "camera3_optical_frame",
+        "camera4": "camera4_optical_frame",
+        "camera5": "camera5_optical_frame",
+    })
+    """URDF link names for each camera's optical frame."""
+
+    tag_poses: dict[str, dict[int, TagPose]] = field(default_factory=dict)
+    """Tag poses for each tag."""
+
+    extrinsics: dict[str, CameraExtrinsics] = field(default_factory=lambda: {
+        "realsense1": CameraExtrinsics(),
+        "realsense2": CameraExtrinsics(),
+        "camera1": CameraExtrinsics(),
+        "camera2": CameraExtrinsics(),
+        "camera3": CameraExtrinsics(),
+        "camera4": CameraExtrinsics(),
+        "camera5": CameraExtrinsics(),
+    })
+    """Extrinsics for each camera."""
+
+    intrinsics: dict[str, CameraIntrinsics] = field(default_factory=lambda: {
+        "realsense1": CameraIntrinsics(
+            fov=1.0,
+            aspect=1.0,
+            fx=1.0,
+            fy=1.0,
+            ppx=1.0,
+            ppy=1.0,
+        ),
+        "realsense2": CameraIntrinsics(
+            fov=1.0,
+            aspect=1.0,
+            fx=1.0,
+            fy=1.0,
+            ppx=1.0,
+            ppy=1.0,
+        ),
+        "camera1": CameraIntrinsics(
+            fov=1.0,
+            aspect=1.0,
+            fx=1.0,
+            fy=1.0,
+            ppx=1.0,
+            ppy=1.0,
+        ),
+        "camera2": CameraIntrinsics(
+            fov=1.0,
+            aspect=1.0,
+            fx=1.0,
+            fy=1.0,
+            ppx=1.0,
+            ppy=1.0,
+        ),
+        "camera3": CameraIntrinsics(
+            fov=1.0,
+            aspect=1.0,
+            fx=1.0,
+            fy=1.0,
+            ppx=1.0,
+            ppy=1.0,
+        ),
+        "camera4": CameraIntrinsics(
+            fov=1.0,
+            aspect=1.0,
+            fx=1.0,
+            fy=1.0,
+            ppx=1.0,
+            ppy=1.0,
+        ),
+        "camera5": CameraIntrinsics(
+            fov=1.0,
+            aspect=1.0,
+            fx=1.0,
+            fy=1.0,
+            ppx=1.0,
+            ppy=1.0,
+        ),
+    })
+    """Intrinsics for each camera."""
--- src/new/utils.py
+++ src/new/utils.py
@@ -1,1 +1,55 @@
-# shared utils for all modules, minimal
+from dataclasses import asdict
+import logging
+import os
+from pprint import pformat
+from typing import Any
+
+import tyro
+
+TIME_FORMAT: str = "%Yy-%mm-%dd-%Hh-%Mm-%Ss"
+LOG_FORMAT: str = "%(asctime)s %(levelname)s: %(message)s"
+SUBMODULES: list[str] = ['_bot', '_ik', '_ink', '_log', '_map', '_net', '_path', '_plan', '_scan', '_tag', '_viz']
+
+# used for gui log text/circles/bboxes
+COLORS: dict[str, tuple[int, int, int]] = {
+    "black":      (  0,   0,   0),
+    "white":      (255, 255, 255),
+    "blue":       ( 82, 153, 224),
+    "green":      ( 82, 224, 105),
+    "red":        (224,  86,  82),
+    "yellow":     (224, 212,  82),
+    "purple":     (189,  82, 224),
+    "orange":     (224, 117,  82),
+    "gold":       (189, 224,  82),
+    "chartreuse": (105, 224,  82),
+    "mint":       ( 82, 224, 177),
+    "teal":       ( 82, 224, 224),
+    "cyan":       ( 82, 189, 224),
+    "indigo":     ( 82,  82, 224),
+    "violet":     (129,  82, 224),
+    "magenta":    (224,  82, 201),
+    "pink":       (224,  82, 129),
+}
+
+def get_logger(name: str) -> logging.Logger:
+    """Get a logger with a specific name."""
+    return logging.getLogger(f"tatbot.{name}")
+
+log = get_logger('_log')
+
+def print_config(args: Any):
+    log.info(f"🛠️ Full Config of type {type(args)}:")
+    log.info(pformat(asdict(args)))
+
+def setup_log_with_config(config: Any) -> Any:
+    args = tyro.cli(config)
+    logging.basicConfig(level=logging.INFO)
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+        for submodule in SUBMODULES:
+            logging.getLogger(f"tatbot.{submodule}").setLevel(logging.DEBUG)
+        log.debug("🐛 Debug mode enabled.")
+    if hasattr(args, "output_dir"):
+        os.makedirs(args.output_dir, exist_ok=True)
+        log.info(f"💾 Saving output to {args.output_dir}")
+    return args
