240452dafb4fc5b94a9ed2768e1cbd190af987c3
hu.po <hugo.p.cmu@gmail.com>
2025-06-28 08:10:48 -0500

    Add Stroke dataclass and update path handling in gen.py; include svgpathtools in workspace

--- src/new/data.py
+++ src/new/data.py
@@ -249,44 +249,6 @@ class Scene:
     """Intrinsics for each camera."""
 
 @dataclass
-class Plan:
-    name: str = "plan"
-    """Name of the plan."""
-
-    dirpath: str = ""
-    """Path to the directory containing the plan files."""
-
-    ink_config: InkConfig = field(default_factory=InkConfig)
-    """Config containig InkCaps and palette position."""
-    bot_config: BotConfig = field(default_factory=BotConfig)
-    """Bot configuration to use for the plan."""
-
-    strokes: dict[str, Stroke] = field(default_factory=dict)
-    """Dictionary of path metadata objects."""
-    path_idx_to_strokes: list[list[Stroke]] = field(default_factory=list)
-    """Map from pathbatch idx to list of strokes that make up that path."""
-
-    image_width_m: float = 0.074 # A7 size
-    """Width of the image in meters."""
-    image_height_m: float = 0.105 # A7 size
-    """Height of the image in meters."""
-    image_width_px: int | None = None
-    """Width of the image in pixels."""
-    image_height_px: int | None = None
-    """Height of the image in pixels."""
-
-    ik_batch_size: int = 1024
-    """Batch size for IK computation."""
-    path_length: int = 108
-    """All paths will be resampled to this length."""
-    path_dt_fast: float = 0.1
-    """Time between poses in seconds for fast movement."""
-    path_dt_slow: float = 2.0
-    """Time between poses in seconds for slow movement."""
-
-
-
-@dataclass
 class Stroke:
     description: str | None = None
     """Natural language description of the path."""
@@ -313,21 +275,48 @@ class Stroke:
     inkcap: str | None = None
     """Name of the inkcap which provided the ink for the stroke."""
 
-    is_completed: bool = False
-    """Whether the path is completed."""
-    completion_time: float | None = None
-    """Time taken to complete the path in seconds."""
+
+@dataclass
+class Plan:
+    name: str = "plan"
+    """Name of the plan."""
+
+    dirpath: str = ""
+    """Path to the directory containing the plan files."""
+
+    strokes: dict[str, Stroke] = field(default_factory=dict)
+    """Dictionary of path metadata objects."""
+    path_idx_to_strokes: list[list[Stroke]] = field(default_factory=list)
+    """Map from pathbatch idx to list of strokes that make up that path."""
+
+    image_width_m: float = 0.074 # A7 size
+    """Width of the image in meters."""
+    image_height_m: float = 0.105 # A7 size
+    """Height of the image in meters."""
+    image_width_px: int | None = None
+    """Width of the image in pixels."""
+    image_height_px: int | None = None
+    """Height of the image in pixels."""
+
+    ik_batch_size: int = 1024
+    """Batch size for IK computation."""
+    path_length: int = 108
+    """All paths will be resampled to this length."""
+    path_dt_fast: float = 0.1
+    """Time between poses in seconds for fast movement."""
+    path_dt_slow: float = 2.0
+    """Time between poses in seconds for slow movement."""
 
 
 @dataclass
 class FullConfig:
-    urdf: URDF = field(default_factory=URDF)
+    urdf: URDF
     """URDF configuration for the robot."""
-    bot: BotConfig = field(default_factory=BotConfig)
+    bot: BotConfig
     """Bot configuration to use for the plan."""
-    ink: InkConfig = field(default_factory=InkConfig)
+    ink: InkConfig
     """Config containig InkCaps and palette position."""
-    tag: TagConfig = field(default_factory=TagConfig)
+    tag: TagConfig
     """Config containing AprilTag parameters."""
-    scan: Scan = field(default_factory=Scan)
+    scan: Scan
     """Scan configuration to use for the plan."""
