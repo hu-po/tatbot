c2bc0885242ef505cb0017291e59f81613a15b30
hu.po <hugo.p.cmu@gmail.com>
2025-07-01 10:59:25 -0500

    asd

--- tatbot/bot/trossen.py
+++ tatbot/bot/trossen.py
@@ -146,11 +146,11 @@ def configure_arm(filepath: str, ip: str, test_pose_name: str, test_tolerance: f
     driver.set_all_modes(trossen_arm.Mode.position)
     sleep_pose = driver.get_all_positions()[:7]
     log.info(f"🦾 sleep pose: {sleep_pose}")
-    test_pose = ArmPose.from_name(test_pose_name)
+    test_pose = ArmPose.from_name(test_pose_name).joints[:7]
     log.info(f"🦾 Testing arm {ip} with pose {test_pose}")
-    driver.set_all_positions(trossen_arm.VectorDouble(test_pose.joints), blocking=True)
+    driver.set_all_positions(trossen_arm.VectorDouble(test_pose), blocking=True)
     current_pose = driver.get_all_positions()[:7]
-    assert np.allclose(current_pose, test_pose.joints, atol=test_tolerance), f"❌🦾 current pose {current_pose} does not match test pose {test_pose}"
+    assert np.allclose(current_pose, test_pose, atol=test_tolerance), f"❌🦾 current pose {current_pose} does not match test pose {test_pose}"
     driver.set_all_positions(trossen_arm.VectorDouble(sleep_pose), blocking=True)
     current_pose = driver.get_all_positions()[:7]
     assert np.allclose(current_pose, sleep_pose, atol=test_tolerance), f"❌🦾 current pose {current_pose} does not match sleep pose {sleep_pose}"
