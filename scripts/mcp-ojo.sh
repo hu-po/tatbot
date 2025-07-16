a7fcf360551f6fb1a72845b771b5d27902cf95b0
hu-po <hu.po.xyz@gmail.com>
2025-07-16 13:13:28 -0500

    asd

--- scripts/mcp-ojo.sh
+++ scripts/mcp-ojo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Starting MCP server on ojo 🦎..."
 source ~/tatbot/scripts/env.sh
+uv pip install .[map]
 uv run -m tatbot.mcp.ojo
--- scripts/mcp-trossen-ai.sh
+++ scripts/mcp-trossen-ai.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Starting MCP server on trossen-ai 🦾..."
 source ~/tatbot/scripts/env.sh
+uv pip install .[bot,cam,viz]
 uv run -m tatbot.mcp.trossen-ai
