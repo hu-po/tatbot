6c1425e40dcd4dda97f7aa639f281a5178d1607b
hu.po <hugo.p.cmu@gmail.com>
2025-07-19 08:49:25 -0500

    asd

--- src/tatbot/viz/rs_pc.py
+++ src/tatbot/viz/rs_pc.py
@@ -1,15 +1,14 @@
 import logging
-import os
 from dataclasses import dataclass
-from typing import Tuple
+from typing import Tuple, Dict
 import time
 
 import numpy as np
+import numpy.typing as npt
 import pyrealsense2 as rs
+import jaxlie
+from viser.extras import PointCloudHandle
 
-from tatbot.bot.urdf import get_link_indices, get_link_poses
-from tatbot.data.pose import ArmPose, Pose
-from tatbot.gen.ik import ik
 from tatbot.utils.log import get_logger, print_config, setup_log_with_config
 from tatbot.viz.base import BaseViz, BaseVizConfig
 
@@ -21,25 +20,21 @@ class RsPcVizConfig(BaseVizConfig):
     debug: bool = False
     """Enable debug logging."""
 
-    realsense_a: RealSenseConfig = RealSenseConfig(serial_number="230422273017", link_name="right/camera_depth_optical_frame")
-    """Configuration for RealSense Camera A (attached to right arm)."""
-    realsense_b: RealSenseConfig = RealSenseConfig(serial_number="218622278376", link_name="cam_high_depth_optical_frame")
-    """Configuration for RealSense Camera B (overhead)."""
+    point_size: float = 0.001
+    """Size of points in the point cloud visualization."""
+    decimation: int = 6
+    """Decimation filter magnitude for depth frames (integer >= 1)."""
+    clipping: Tuple[float, float] = (0.03, 0.8)
+    """Clipping range for depth in meters (min, max)."""
 
 @dataclass
 class RealSenseConfig:
     fps: int = 5
     """Frames per second for the RealSense camera."""
-    point_size: float = 0.001
-    """Size of points in the point cloud visualization."""
     serial_number: str = ""
     """Serial number of the RealSense camera device."""
     link_name: str = ""
     """Name of the camera link in the robot URDF."""
-    decimation: int = 6
-    """Decimation filter magnitude for depth frames (integer >= 1)."""
-    clipping: Tuple[float, float] = (0.03, 0.8)
-    """Clipping range for depth in meters (min, max)."""
 
 class RealSenseCamera:
     def __init__(self, config: RealSenseConfig):
@@ -100,67 +95,38 @@ class RsPcViz(BaseViz):
 
         self.realsense_cams: Dict[str, RealSenseCamera] = {}
         self.realsense_pointclouds: Dict[str, PointCloudHandle] = {}
-        for realsense_camera in self.scene.cams.realsenses:
-            self.realsense_cams[realsense_camera.name] = RealSenseCamera(
+        for i, realsense in enumerate(self.scene.cams.realsenses):
+            self.realsense_cams[realsense.name] = RealSenseCamera(
                 RealSenseConfig(
-                    fps=realsense_camera.fps,
-                    point_size=realsense_camera.point_size,
-                    serial_number=realsense_camera.serial_number,
-                    link_name=realsense_camera.link_name,
-                    frustrum_scale=realsense_camera.frustrum_scale,
+                    fps=realsense.fps,
+                    serial_number=realsense.serial_number,
+                    link_name=self.scene.urdf.cam_link_names[i],
                 )
             )
-            self.realsense_pointclouds[realsense_camera.name] = self.server.scene.add_point_cloud(
-                f"/pointcloud_{realsense_camera.name}",
+            self.realsense_pointclouds[realsense.name] = self.server.scene.add_point_cloud(
+                f"/pointcloud_{realsense.name}",
                 points=np.zeros((1, 3)),
                 colors=np.zeros((1, 3), dtype=np.uint8),
-                point_size=realsense_camera.point_size,
+                point_size=config.point_size,
             )
 
     def step(self):
-        for rs_name in self.scene.cams.realsenses:
+        for realsense in self.scene.cams.realsenses:
             realsense_start_time = time.time()
-            rs_config = self.scene.cams.realsenses[rs_name]
-            rs_camera = RealSenseCamera(rs_config)
-            rgb, positions, colors = rs_camera.make_observation()
-            self.realsense_frustrums[rs_name].image = rgb
-            
+            rgb, positions, colors = self.realsense_cams[realsense.name].make_observation()
+            self.realsense_frustrums[realsense.name].image = rgb
+
+            # forward kinematics to get realsense pose
             camera_link_idx_a = self.robot.links.names.index(config.realsense_a.link_name)
             joint_array = np.concatenate([joint_pos_current.left, joint_pos_current.right])
             camera_pose_a = self.robot.forward_kinematics(joint_array)[camera_link_idx_a]
-            realsense_a_frustrum.wxyz = camera_pose_a[:4]
-            realsense_a_frustrum.position = camera_pose_a[-3:]
-            camera_transform_a = jaxlie.SE3(camera_pose_a)
-            positions_world_a = camera_transform_a @ positions_a
-            pointcloud_a.points = np.array(positions_world_a)
-            pointcloud_a.colors = np.array(colors_a)
             
-            log.info(f"🔍 {rs_name} took {time.time() - realsense_start_time} seconds")
-
-
-        # ORIGINAL CODE BELOW - REFERENCE ONLY
-        realsense_start_time = time.time()
-        rgb_a, positions_a, colors_a = self.realsense_a.make_observation()
-        self.realsense_a_frustrum.image = rgb_a
-        rgb_b, positions_b, colors_b = self.realsense_b.make_observation()
-        realsense_b_frustrum.image = rgb_b
-        camera_link_idx_a = robot.links.names.index(config.realsense_a.link_name)
-        joint_array = np.concatenate([joint_pos_current.left, joint_pos_current.right])
-        camera_pose_a = robot.forward_kinematics(joint_array)[camera_link_idx_a]
-        realsense_a_frustrum.wxyz = camera_pose_a[:4]
-        realsense_a_frustrum.position = camera_pose_a[-3:]
-        realsense_b_frustrum.wxyz = camera_pose_b_static[:4]
-        realsense_b_frustrum.position = camera_pose_b_static[-3:]
-        camera_transform_a = jaxlie.SE3(camera_pose_a)
-        camera_transform_b = jaxlie.SE3(camera_pose_b_static)
-        positions_world_a = camera_transform_a @ positions_a
-        positions_world_b = camera_transform_b @ positions_b
-        pointcloud_a.points = np.array(positions_world_a)
-        pointcloud_a.colors = np.array(colors_a)
-        pointcloud_b.points = np.array(positions_world_b)
-        pointcloud_b.colors = np.array(colors_b)
-        realsense_elapsed_time = time.time() - realsense_start_time
-        realsense_duration_ms.value = realsense_elapsed_time * 1000
+            # update pointcloud
+            positions_world = jaxlie.SE3(camera_pose) @ positions
+            self.realsense_pointclouds[realsense.name].points = np.array(positions_world)
+            self.realsense_pointclouds[realsense.name].colors = np.array(colors)
+            
+            log.info(f"🔍 {realsense.name} took {time.time() - realsense_start_time} seconds")
 
 
 if __name__ == "__main__":
