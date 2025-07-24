f24b196eb295e70597ac5f5907b5feeb5553c479
hu.po <hugo.p.cmu@gmail.com>
2025-07-24 12:18:45 -0500

    asd

--- src/tatbot/ops/__init__.py
+++ src/tatbot/ops/__init__.py
@@ -14,13 +14,13 @@ def get_op(op_name: str, node_name: str) -> tuple[BaseOp, BaseOpConfig]:
     if op_name == "base":
         return BaseOp, BaseOpConfig
     elif op_name == "align":
-        from tatbot.ops.align import AlignOp, AlignOpConfig
+        from tatbot.ops.record_align import AlignOp, AlignOpConfig
         return AlignOp, AlignOpConfig
     elif op_name == "sense":
-        from tatbot.ops.sense import SenseOp, SenseOpConfig
+        from tatbot.ops.record_sense import SenseOp, SenseOpConfig
         return SenseOp, SenseOpConfig
     elif op_name == "stroke":
-        from tatbot.ops.stroke import StrokeOp, StrokeOpConfig
+        from tatbot.ops.record_stroke import StrokeOp, StrokeOpConfig
         return StrokeOp, StrokeOpConfig
     else:
         raise ValueError(f"Unknown operation: {op_name}")
