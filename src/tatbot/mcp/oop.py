e16f584ccde8c000db765b3e64694d4a7e02fce6
hu.po <hugo.p.cmu@gmail.com>
2025-07-27 08:14:09 -0500

    asd

---
+++ src/tatbot/mcp/oop.py
@@ -0,0 +1,25 @@
+"""MCP server running on oop node."""
+
+import logging
+
+from mcp.server.fastmcp import Context, FastMCP
+
+from tatbot.mcp.base import MCPConfig, _run_op
+from tatbot.utils.log import get_logger, print_config, setup_log_with_config
+
+log = get_logger("mcp.oop", "🔌🦊")
+
+mcp = FastMCP("tatbot.oop", host="192.168.1.51", port=8000)
+
+@mcp.tool()
+async def run_op(op_name: str, ctx: Context) -> str:
+    """Runs an operation, yields intermediate results, see available ops in tatbot.ops module."""
+    return await _run_op(op_name, ctx, "oop")
+
+
+if __name__ == "__main__":
+    args = setup_log_with_config(MCPConfig)
+    if args.debug:
+        log.setLevel(logging.DEBUG)
+    print_config(args, log)
+    mcp.run(transport=args.transport)
