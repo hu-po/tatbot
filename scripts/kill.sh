d5082734d8c7d7a03fca6654a80e5cee0ebff623
hu.po <hugo.p.cmu@gmail.com>
2025-07-29 09:22:24 -0500

    keel

---
+++ scripts/kill.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "☠️ Killing all tatbot processes"
+sudo pkill -9 -f tatbot
+echo "✅ Done"
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo "🧹 Linting and formatting code"
 uv run isort .
 uv run ruff check --config pyproject.toml --fix
+echo "✅ Done"
