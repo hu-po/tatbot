dba09adc81ab588eb1ad06e28dd5e973224f1d7b
hu-po <hu.po.xyz@gmail.com>
2025-08-01 15:39:29 -0500

    asd

--- scripts/poweroff-nodes.sh
+++ scripts/poweroff-nodes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh rpi2 "poweroff"
-ssh rpi1 "poweroff"
-ssh trossen-ai "poweroff"
-ssh ojo "poweroff"
+ssh rpi2 "sudo poweroff"
+ssh rpi1 "sudo poweroff"
+ssh trossen-ai "sudo poweroff"
+ssh ojo "sudo poweroff"
