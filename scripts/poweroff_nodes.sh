6343ac2b7a9bdeaa21e58baf811a09be65e422ad
hu.po <hugo.p.cmu@gmail.com>
2025-08-11 14:25:38 -0500

    asd

--- scripts/poweroff_nodes.sh
+++ scripts/poweroff_nodes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ssh eek "sudo poweroff"
-ssh hog "sudo poweroff"
-ssh rpi2 "sudo poweroff"
-ssh rpi1 "sudo poweroff"
 ssh ojo "sudo poweroff"
+ssh hog "sudo poweroff"
+ssh rpi1 "sudo poweroff"
+ssh rpi2 "sudo poweroff"
