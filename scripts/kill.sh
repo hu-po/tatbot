fc12771b63b8f5946ac862ab35d9910caae08689
hu-po <hu.po.xyz@gmail.com>
2025-08-25 10:52:39 -0500

    asd

--- scripts/kill.sh
+++ scripts/kill.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "☠️  Killing all tatbot MCP related processes"
 pkill -9 -f "tatbot.mcp" || true
+echo "☠️  Killing all python processes"
+pkill -9 -f "python" || true
 echo "✅ Done"
